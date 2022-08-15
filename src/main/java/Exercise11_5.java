79cd44cba324a9e82338eadf524e415dde3eaca9
KyungTae Bae <kyungtae@marvrus.com>
2022-08-15 18:38:48 +0900

    자바의 정석 연습문제 11-5

---
+++ src/main/java/Exercise11_5.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Student implements Comparable {
+    String name;
+    int ban;
+    int no;
+    int kor, eng, math;
+    Student(String name, int ban, int no, int kor, int eng, int math) {
+        this.name = name;
+        this.ban = ban;
+        this.no = no;
+        this.kor = kor;
+        this.eng = eng;
+        this.math = math;
+    }
+    int getTotal() {
+        return kor+eng+math;
+    }
+    float getAverage() {
+        return (int)((getTotal()/ 3f)*10+0.5)/10f;
+    }
+    public String toString() {
+        return name +","+ban +","+no +","+kor +","+eng +","+math
+                +","+getTotal() +","+getAverage();
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if(o instanceof Student) {
+            Student tmp = (Student)o;
+            return name.compareTo(tmp.name); // String compareTo() 클래스의 를 호출
+        } else {
+            return -1;
+        }
+    }
+}
+
+class Exercise11_5 {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add(new Student("hong",1,1,100,100,100));
+        list.add(new Student("nam",1,2,90,70,80));
+        list.add(new Student("kim",1,3,80,80,90));
+        list.add(new Student("lee",1,4,70,90,70));
+        list.add(new Student("ahn",1,5,60,100,80));
+
+        Collections.sort(list);
+        Iterator it = list.iterator();
+
+        while(it.hasNext())
+            System.out.println(it.next());
+    }
+}
