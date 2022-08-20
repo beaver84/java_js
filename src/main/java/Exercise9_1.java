ceb9e55eaa1ac1da2fbd89af54714ec47998ffd6
Kyungtae Bae <Kyungtae@marvrus.com>
2022-08-20 10:51:48 +0900

    자바의 정석 예제 9-1

---
+++ src/main/java/Exercise9_1.java
@@ -0,0 +1,36 @@
+public class Exercise9_1 {
+    public static void main(String[] args) {
+        SutdaCard2 c1 = new SutdaCard2(3, true);
+        SutdaCard2 c2 = new SutdaCard2(3, true);
+
+        System.out.println("c1=" + c1);
+        System.out.println("c2=" + c2);
+        System.out.println("c1.equals(c2):" + c1.equals(c2));
+    }
+}
+
+class SutdaCard2 {
+    int num;
+    boolean isKwang;
+
+    SutdaCard2(){
+        this(1, true);
+    }
+
+    SutdaCard2(int num, boolean isKwang) {
+        this.num = num;
+        this.isKwang = isKwang;
+    }
+
+    public boolean equals(Object obj) {
+        if(obj!=null && obj instanceof SutdaCard2){
+            return num == ((SutdaCard2) obj).num && isKwang == ((SutdaCard2) obj).isKwang;
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        return num + (isKwang ? "K" : "");
+    }
+}
