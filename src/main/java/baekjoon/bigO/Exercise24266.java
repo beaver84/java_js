7f106345a002a5256a4085dccaebc0f97685df3c
baekyungtae <baekyungtae@baekyungtae-MacBookPro.local>
2023-08-17 16:49:52 +0900

    백준 빅O 문제풀이

---
+++ src/main/java/baekjoon/bigO/Exercise24266.java
@@ -0,0 +1,18 @@
+package baekjoon.bigO;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Exercise24266 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        long n = Long.parseLong(br.readLine());
+
+        System.out.println(n * n * n);
+        System.out.println(3);
+    }
+}
