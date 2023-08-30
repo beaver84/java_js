1780f204046336b7186c24b9ebab1fe4b8ad6d1e
배경태SD3 <kyungtae.bae@timf.co.kr>
2023-08-30 14:51:13 +0900

    인프런 알고리즘 문자열 문제풀이 저장 11

---
+++ src/main/java/inflearn/string/InfExercise1By11.java
@@ -0,0 +1,18 @@
+package inflearn.string;
+
+import java.util.Scanner;
+
+public class InfExercise1By11 {
+    public String solution(String str){
+        String answer = "";
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        InfExercise1By11 T = new InfExercise1By11();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.print(str);
+    }
+}
