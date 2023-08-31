e2d85d58204772f190cb5d7bf9dedc9da040dac9
bae kyungtae <b841010@naver.com>
2023-08-31 22:07:39 +0900

    인프런 배열 문제풀이1

--- src/main/java/inflearn/array/InfExercise2By1.java
+++ src/main/java/inflearn/array/InfExercise2By1.java
@@ -8,6 +8,11 @@ public class InfExercise2By1 {
 
     public ArrayList<Integer> solution(int n, int[] arr) {
         ArrayList<Integer> answer = new ArrayList<>();
+        answer.add(arr[0]);
+        for (int i = 1; i < n; i++) {
+            if(arr[i]>arr[i-1]) answer.add(arr[i]);
+        }
+
         return answer;
     }
 
