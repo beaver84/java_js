bd7bdcdba40aba0b264a68c3de8d321d8b75b765
bae kyungtae <b841010@naver.com>
2023-12-13 22:46:52 +0900

    김영한 자바 기본 - 객체지향프로그래밍 2번째 연습문제

--- src/main/java/java_basic/oop/ex/Account.java
+++ src/main/java/java_basic/oop/ex/Account.java
@@ -2,12 +2,13 @@ package java_basic.oop.ex;
 
 public class Account {
     int balance;
-    int deposit(int amount){
+
+    int deposit(int amount) {
         return this.balance = this.balance + amount;
     }
 
     int withdraw(int amount) {
-        if(this.balance - amount < 0){
+        if (this.balance - amount < 0) {
             System.out.println("잔액 부족");
             return this.balance;
         }
